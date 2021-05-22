610e269d3da7bbfc0fb28b814a8b9d79beadd10c
Đức Nguyễn <ntd275@gmail.com>
2021-05-22 15:58:25 +0700

    a

--- Demo/src/App.js
+++ Demo/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import data from './data/data'
-
+import { Form } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends React.Component {
+  getStoreOption = () => {
+    let options = []
+    console.log(data)
+    for (let key in data) {
+      options.push(
+        <option key={key} value={key}>{key}</option>
+      )
+    }
+    return options
+  }
   render() {
     return (
-      <div>
-        {JSON.stringify(data)}
+      <div className="container">
+        <Form.Group controlId="exampleForm.ControlSelect1">
+          <Form.Label>Store</Form.Label>
+          <Form.Control as="select">
+            {this.getStoreOption()}
+          </Form.Control>
+        </Form.Group>
       </div>
     )
   }
