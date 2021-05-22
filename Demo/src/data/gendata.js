2944d445f68cd0ef73476b5c48527ecbf34a47e4
Đức Nguyễn <ntd275@gmail.com>
2021-05-22 15:27:10 +0700

    a

--- Demo/src/data/gendata.js
+++ Demo/src/data/gendata.js
@@ -5,12 +5,14 @@ let data = {}
 const createData = async () => {
     let arr = await csv().fromFile('./stores.csv');
     for (let i = 0; i < arr.length; i++) {
-        data[arr[i].Store] = []
+        data[arr[i].Store] = {}
     }
-    arr = await csv().fromFile('./train.csv')
+    arr = await csv().fromFile('./test.csv')
     for (let i = 0; i < arr.length; i++) {
-        if (!data[arr[i].Store].includes(arr[i].Dept)) {
-            data[arr[i].Store].push(arr[i].Dept)
+        if (data[arr[i].Store][arr[i].Dept]) {
+            data[arr[i].Store][arr[i].Dept].push(arr[i].Date)
+        } else {
+            data[arr[i].Store][arr[i].Dept] = []
         }
     }
     fs.writeFileSync('./data.js', 'let Data =' + JSON.stringify(data) + '; export default Data');
